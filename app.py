b73615525e212ad7fbd26d90f9b29702cfad4350
COFE App <cofeapp@COFEs-MacBook-Pro.local>
2026-04-06 17:02:47 +0400

    Fix: add environment variables support in app.py

--- app.py
+++ app.py
@@ -2,6 +2,14 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# Мы заставляем Flask брать настройки из окружения (из твоего YAML или Docker)
+# Если переменной нет, он возьмет значение по умолчанию (например, 'localhost')
+app.config['MYSQL_HOST'] = os.environ.get('MYSQL_HOST', 'mysql-service')
+app.config['MYSQL_USER'] = os.environ.get('MYSQL_USER', 'root')
+app.config['MYSQL_PASSWORD'] = os.environ.get('MYSQL_PASSWORD', 'mypassword')
+app.config['MYSQL_DB'] = os.environ.get('MYSQL_DB', 'dockerdb')
+# ---------------------------
+
 @app.route('/')
 def hello():
     return 'Docker KP2 task 1 says: You did it! Well done!'
