d1e1a902b6e12f3638c4578dd6f965d7654ce401
COFE App <cofeapp@COFEs-MacBook-Pro.local>
2026-04-06 16:43:04 +0400

    Add automated tests and update QA-Ops pipeline

---
+++ tests/test_app.py
@@ -0,0 +1,17 @@
+import pytest
+from app import app  # Импортируем Flask-апп
+
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
+
+def test_homepage_status_code(client):
+    """Тест проверяет, что главная страница доступна (200 OK)"""
+    response = client.get('/')
+    assert response.status_code == 200
+
+def test_db_connection_logic():
+    """Пример теста логики: проверяем, что приложение вообще пытается стучаться в БД"""
+    # проверкa конфигов
+    assert app.config['MYSQL_HOST'] == 'mysql-service'
